24bd4027eb88cfc0965ea9d9e272dc94f5f7ec54
yeozhenhao <elgeneyeo@hotmail.com>
2022-07-08 00:17:24 +0800

    IMPT UPDATE: 1. Upgraded image comparison function to use ImageChops.difference() to generate a new image showing the difference, then instead of using a bounding box method to calculate similarity, we use the percentage black to calculate similarity

--- autoscreenshotter.py
+++ autoscreenshotter.py
@@ -1,4 +1,7 @@
+import copy
+
 import pyautogui
+import keyboard
 import scipy
 from PIL import ImageChops, Image
 import time
@@ -13,17 +16,32 @@ from scipy.linalg import norm
 
 import numpy as np
 
-SS_size_x, SS_size_y = (900, 510) # Default is 900, 510
-SS_loc_offset_x, SS_loc_offset_y = (50, 280) # Default is 50, 280
+HOTKEY_MODE = False
+hotkey_to_screenshot = 'ctrl+l'
 
-pause_time = 3.5 # seconds
+## Options to modify image arrays before comparing
+black_threshold = 10 ## Default = 50
+percentage_black_threshold = 0.95
+SS_size_x, SS_size_y = (890, 520) # Default 740, 550
+SS_loc_offset_x, SS_loc_offset_y = (0, 160) # Default is 50, 280 #120, 220
+
+pause_time = 4.5 # seconds
+
+resize_img_smaller = False
+resize_to = (100, 56)
+convert_img_to_grayscale = False
+normalise_img = False
+
+## USELESS VARIABLES - IGNORE
+# z_norm_diff_cutoff = 1900
+fail_safe_boolean = False # When fail-safe mode is True, moving the mouse to the upper-left will raise a pyautogui.FailSafeException that can abort your program:
 
 zoom_pic_file_loc = 'pics\zoomicon.png'
 Word_save_icon_file_loc = 'pics\saveicon.png'
 
-def send_to_clipboard(PIL_image):
+def send_to_clipboard(PIL_img):
     output = BytesIO()
-    PIL_image.convert('RGB').save(output, 'BMP')
+    PIL_img.convert('RGB').save(output, 'BMP')
     data = output.getvalue()[14:]
     output.close()
 
@@ -32,15 +50,14 @@ def send_to_clipboard(PIL_image):
     win32clipboard.SetClipboardData(win32clipboard.CF_DIB, data)
     win32clipboard.CloseClipboard()
 
-def screenshot_and_save_to_word(screenshot):
+def screenshot_and_save_to_word(PIL_img):
     old_cursor_position = pyautogui.position()
-    send_to_clipboard(screenshot)
-    print(f"Looking for image on screen, returning output: ")
+    send_to_clipboard(PIL_img)
     loc = pyautogui.locateOnScreen(Word_save_icon_file_loc, grayscale=False, confidence=.9) # returns (left, top, width, height) of first place it is found
-    print(loc)
+    print(f"Looking for Word Doc image on screen, returning output: {loc}")
     pyautogui.moveTo(loc)
     # pyautogui.moveRel(0, 0, duration=0.1)
-    pyautogui.click(clicks=2, interval=0, button='left') #
+    pyautogui.click(clicks=1, interval=0, button='left')
     pyautogui.moveTo(old_cursor_position)
     pyautogui.press('enter')
     pyautogui.hotkey('ctrl', 'v')
@@ -50,8 +67,8 @@ def screenshot_and_save_to_word(screenshot):
 ## SOURCE: https://stackoverflow.com/questions/189943/how-can-i-quantify-difference-between-two-images
 def to_grayscale(arr):
     "If arr is a color image (3D array), convert it to grayscale (2D array)."
-    print(f"arr: {arr}")
-    print(f"arr: {arr}")
+    # print(f"arr: {arr}")
+    # print(f"arr: {arr}")
     if len(arr.shape) == 3:
         return np.average(arr, -1)  # average over the last axis (color channels)
     else:
@@ -71,25 +88,37 @@ def normalize(arr):
     return (arr-amin)*255/rng
 
 
-def compare_images(img_1, img_2): ## img_1 and img_2 are PIL images
+def compare_images_using_norms(PIL_img_1, PIL_img_2): ## img_1 and img_2 are PIL images
     # Convert PIL to arrays using numpy first
-    img_1_arr = np.array(img_1)
-    img_2_arr = np.array(img_2)
+    img_1_arr = np.array(PIL_img_1)
+    img_2_arr = np.array(PIL_img_2)
     # print(f"img_1_arr: {img_1_arr}")
     # print(f"img_2_arr: {img_2_arr}")
-    # normalize to compensate for exposure difference, this may be unnecessary
-    # consider disabling it
-    img_1_normalized = normalize(img_1_arr)
-    img_2_normalized = normalize(img_2_arr)
-    # print(f"img_1_normalized: {img_1_normalized}")
-    # print(f"img_2_normalized: {img_2_normalized}")
-    # calculate the difference and its norms
-    diff_normalized = img_1_normalized - img_2_normalized  # elementwise for scipy arrays
-    # print(f"diff_normalized: {diff_normalized}")
-    m_norm = sum(abs(diff_normalized))  # Manhattan norm
+
+    # ## grayscale
+    if convert_img_to_grayscale is True:
+        print(f"Converting images to grayscale...")
+        img_1_arr = to_grayscale(img_1_arr)
+        img_2_arr = to_grayscale(img_2_arr)
+        # print(f"img_1_arr_grayscale: {img_1_arr}")
+        # print(f"img_2_arr_grayscale: {img_2_arr}")
+
+    ## normalize to compensate for exposure difference, this may do more wrong than good. For example, a single bright pixel on a dark background will make the normalized image very different
+    ## consider disabling it
+    if normalise_img is True:
+        print(f"Normalizing images...")
+        img_1_arr = normalize(img_1_arr)
+        img_2_arr = normalize(img_2_arr)
+        # print(f"img_1_arr_normalized: {img_1_arr}")
+        # print(f"img_2_arr_normalized: {img_2_arr}")
+
+    ## calculate the difference and its norms
+    diff = img_1_arr - img_2_arr  # elementwise for scipy arrays
+    # print(f"diff_in_arrays: {diff}")
+    m_norm = sum(abs(diff))  # Calculate Manhattan norm of diff
     # print(f"Manhattan norm without sum: {abs(diff_normalized)}")
-    print(f"Manhattan norm: {m_norm}")
-    z_norm = norm(diff_normalized.ravel(), 0)  # Zero norm. .ravel() combines arrays into a single array. norm() calculates the Frobenius norm (aka Euclidean norm) of an array, and it will be the same even if the single array was reshaped into multiple arrays.
+    # print(f"Manhattan norm: {m_norm}")
+    z_norm = norm(diff.ravel(), 0)  # Calculate Zero norm of diff; .ravel() combines arrays into a single array. norm() calculates the Frobenius norm (aka Euclidean norm) of an array, and it will be the same even if the single array was reshaped into multiple arrays.
     print(f"Zero norm: {z_norm}")
     return (m_norm, z_norm)
 
@@ -105,17 +134,75 @@ def compare_images(img_1, img_2): ## img_1 and img_2 are PIL images
     # print(f"PERCENTAGE DIFFERENCE BTWN IMAGES: {percentage_diff}")
 
 
+def resize_images(PIL_img): ## accepts non-array images
+    img_array = np.array(PIL_img)
+    resized_image = cv2.resize(img_array, resize_to, interpolation=cv2.INTER_LINEAR)
+    return resized_image
+
+def return_diff_of_images_with_ImageChops(PIL_img_1, PIL_img_2): ## returns a PIL image showing the difference in pixels (if mostly similar, returns mostly black)
+    if convert_img_to_grayscale is True:
+        print(f"Converting images to grayscale...")
+        PIL_img_1 = to_grayscale(PIL_img_1)
+        PIL_img_2 = to_grayscale(PIL_img_2)
+
+    ## normalize to compensate for exposure difference, this may do more wrong than good. For example, a single bright pixel on a dark background will make the normalized image very different
+    ## consider disabling it
+    if normalise_img is True:
+        print(f"Normalizing images...")
+        PIL_img_1 = normalize(PIL_img_1)
+        PIL_img_2 = normalize(PIL_img_2)
+
+    diff = ImageChops.difference(PIL_img_1, PIL_img_2)
+    print(f"diff: {diff}")
+
+    ## Bounding box method en-boxes all non-zero pixels, and returns a Box PIL object that contains the 3D dimensions
+    ## However, this method is too sensitive; even when images are mostly similar, the Box dimensions could still be very big!
+    # bbox = diff.getbbox() ## bounding box
+    # print(f"bbox: {bbox}")
+    # if bbox is None:  # Next screenshot is similar to previous screenshot
+    #     print("...New screenshot is identical to old screenshot. Pausing before checking again...\n")
+    #     continue
+    return diff
+
+
+def calculate_percentage_black(PIL_img, black_threshold):
+    pixels = PIL_img.getdata()  # get the pixels as a flattened sequence
+    nblack = 0
+    for pixel in pixels:
+        if sum(pixel) < black_threshold:
+            nblack += 1
+    n = len(pixels)
+    percentage_black = (nblack / float(n))
+    print(f"nblack: {nblack}; float(n): {float(n)}; %_black: {percentage_black}")
+    return percentage_black
+
+
 if __name__ == "__main__":
     print(f"Current screen size is {pyautogui.size()}\n") # current screen resolution width and height
     # pyautogui.PAUSE = 2.5 # Set up a 2.5 second pause after each PyAutoGUI call
-    pyautogui.FAILSAFE = True # When fail-safe mode is True, moving the mouse to the upper-left will raise a pyautogui.FailSafeException that can abort your program:
+    pyautogui.FAILSAFE = fail_safe_boolean # When fail-safe mode is True, moving the mouse to the upper-left will raise a pyautogui.FailSafeException that can abort your program:
       # current mouse x and y
     screenshot_count = 0
+    if HOTKEY_MODE is True:
+        while True:
+            print(f"HOTKEY_MODE is True: press {hotkey_to_screenshot} to screenshot!")
+            try:  # used try so that if user pressed other than the given key error will not be shown
+                if keyboard.is_pressed(hotkey_to_screenshot):  # if key 'q' is pressed
+                    print('You pressed a Key!')
+                    loc = pyautogui.locateOnScreen(zoom_pic_file_loc, grayscale=False, confidence=.9)  # returns (left, top, width, height) of first place it is found
+                    if loc is None:
+                        print(f"Zoom image not found. \n")
+                        continue
+                    im = pyautogui.screenshot(region=(loc[0] + SS_loc_offset_x, loc[1] + SS_loc_offset_y, SS_size_x, SS_size_y))
+                    screenshot_and_save_to_word(im)
+                    screenshot_count += 1
+                    print(f"-----Screenshot No. {screenshot_count}  is saved-----\n")
+                    continue  # finishing the loop
+            except:
+                continue  # if user pressed a key other than the given key the loop will continue/break
     while screenshot_count == 0:
-        print(f"Looking for zoom image on screen: ")
         loc = pyautogui.locateOnScreen(zoom_pic_file_loc, grayscale=False, confidence=.9) # returns (left, top, width, height) of first place it is found
-        # print(loc2)
-        # pyautogui.moveTo(loc2) #, duration=0.1)
+        print(f"Looking for zoom image on screen: {loc}")
         if loc is None:
             print(f"Zoom image not found. Pausing before trying again\n")
             time.sleep(pause_time)
@@ -125,36 +212,81 @@ if __name__ == "__main__":
         screenshot_count += 1
         print(f"-----Screenshot No. {screenshot_count}  is saved-----\n")
 
+    # while screenshot_count > 0:
+    #     # pyautogui.PAUSE = pause_time
+    #     time.sleep(pause_time)
+    #     print("====Checking if new screenshot is different:====")
+    #     print(f"Looking for zoom image on screen: ")
+    #     loc = pyautogui.locateOnScreen(zoom_pic_file_loc, grayscale=False, confidence=.9)  # returns (left, top, width, height) of first place it is found
+    #     if loc is None:
+    #         print(f"Zoom image not found. Pausing before trying again\n")
+    #         continue
+    #     im_next = pyautogui.screenshot(region=(loc[0] + SS_loc_offset_x, loc[1] + SS_loc_offset_y, SS_size_x, SS_size_y))
+    #
+    #     if resize_img_smaller is False:
+    #         m_norm, z_norm = compare_images_using_norms(im, im_next)
+    #     else:
+    #         im_downsized = resize_images(im)
+    #         im_next_downsized = resize_images(im_next)
+    #         m_norm, z_norm = compare_images_using_norms(im_downsized, im_next_downsized)
+    #
+    #     if z_norm < z_norm_diff_cutoff: # Next screenshot is similar to previous screenshot
+    #         print("...New screenshot is identical to old screenshot. Pausing before checking again...\n")
+    #         continue
+    #     else:
+    #         im = copy.deepcopy(im_next)
+    #         screenshot_and_save_to_word(im)
+    #         screenshot_count += 1
+    #         print(f"-----New screenshot != previous screenshot.-----")
+    #         print(f"-----Screenshot No. {screenshot_count}  is saved-----\n")
+    #         continue
+
+
     while screenshot_count > 0:
         # pyautogui.PAUSE = pause_time
         time.sleep(pause_time)
         print("====Checking if new screenshot is different:====")
-        print(f"Looking for zoom image on screen: ")
-        loc = pyautogui.locateOnScreen(zoom_pic_file_loc, grayscale=False,
-                                        confidence=.9)  # returns (left, top, width, height) of first place it is found
-        # print(loc2)
-        # pyautogui.moveTo(loc2) #, duration=0.1)
+        loc = pyautogui.locateOnScreen(zoom_pic_file_loc, grayscale=False,confidence=.9)  # returns (left, top, width, height) of first place it is found
+        print(f"Looking for zoom image on screen: {loc}")
         if loc is None:
             print(f"Zoom image not found. Pausing before trying again\n")
             continue
         im_next = pyautogui.screenshot(region=(loc[0] + SS_loc_offset_x, loc[1] + SS_loc_offset_y, SS_size_x, SS_size_y))
-        m_norm, z_norm = compare_images(im, im_next)
-        if z_norm < 5000: # Next screenshot is similar to previous screenshot
-            print("...New screenshot is identical to old screenshot. Pausing before checking again...\n")
+
+        if resize_img_smaller is True:
+            im_next_downsized = resize_images(im_next)
+            im_downsized = resize_images(im)
+            diff = return_diff_of_images_with_ImageChops(im_downsized, im_next_downsized)
+        else:
+            diff = return_diff_of_images_with_ImageChops(im, im_next)
+        percentage_black = calculate_percentage_black(diff, black_threshold)
+        if percentage_black < percentage_black_threshold:
+            im = copy.deepcopy(im_next)
+            screenshot_and_save_to_word(im_next)
+            screenshot_count += 1
+            print(f"-----%_black: {percentage_black}; threshold reached! New screenshot != previous screenshot.-----")
+            print(f"-----Screenshot No. {screenshot_count}  is saved-----\n")
             continue
         else:
-            im = im_next
-            screenshot_and_save_to_word(im)
-            screenshot_count += 1
-            print(f"-----New screenshot is different to old screenshot.-----")
-            print(f"-----Screenshot No. {screenshot_count}  is saved-----\n")
-
+            print("...New screenshot is identical to old screenshot. Pausing before checking again...\n")
             continue
-        break
+
+
+            # if resize_img_smaller is True:
+            #     im_downsized = resize_images(im)
+            #
+            # screenshot_count += 1
+            # print(f"-----New screenshot is different to old screenshot.-----")
+            # print(f"-----Screenshot No. {screenshot_count}  is saved-----\n")
+
+
+
+
+
         # diff = ImageChops.difference(im, im_next)
         # bbox = diff.getbbox()
         # if bbox is not None: # exact comparison
-        #     im = im_next
+        #     im = copy.deepcopy(im_next)
         #     screenshot_and_save_to_word(im)
         #     print("-----New screenshot is different to old screenshot. Pasting in Word doc-----")
         #     continue
