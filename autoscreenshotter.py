16559bbdb983b88ff588c4ca566760f933a64b79
Elgene Yeo <elgeneyeo@hotmail.com>
2023-08-08 13:50:59 +0800

    Updated zoom icon

--- autoscreenshotter.py
+++ autoscreenshotter.py
@@ -18,17 +18,17 @@ import numpy as np
 
 
 HOTKEY_MODE = True
-hotkey_to_screenshot = 'ctrl+l'
+hotkey_to_screenshot = 'ctrl+;'
 
 ## Options to modify image arrays before comparing
 black_threshold = 50 ## Default = 50
 percentage_black_threshold = 0.980 ## Default = 0.98
 
-SS_size_x, SS_size_y = (880, 465) # Default 740
+SS_size_x, SS_size_y = (770, 585) # Default 740
 # , 550
-SS_loc_offset_x, SS_loc_offset_y = (10, 135) # Default is 50, 280 #120, 220
+SS_loc_offset_x, SS_loc_offset_y = (140, 180) # Default is 50, 280 #120, 220
 
-pause_time = 3.0 # seconds before it checks again
+pause_time = 6.0 # seconds before it checks again
 
 zoom_pic_file_loc = 'pics\zoomicon.png'
 Word_save_icon_file_loc = 'pics\saveicon.png'
@@ -187,6 +187,7 @@ if __name__ == "__main__":
     screenshot_count = 0
     if HOTKEY_MODE is True:
         while True:
+            time.sleep(0.3)
             time.sleep(0.8)
             print(f"HOTKEY_MODE is True: press {hotkey_to_screenshot} to screenshot!")
             try:  # used try so that if user pressed other than the given key error will not be shown
